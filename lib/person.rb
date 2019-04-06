90f1467ec1950b613741bd4e5172e7e07f300bbe
Shoptorshi Jamil <sjami002@gmail.com>
2019-04-06 23:02:47 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -75,8 +75,8 @@ end
       person.happiness = person.happiness-2
       return "blah blah partisan blah lobbyist"
     elsif topic == "weather"
-      self.happiness = @happiness+2
-      person.happiness = person.happiness+2
+      self.happiness = @happiness+1
+      person.happiness = person.happiness+1
       return "blah blah sun blah rain"
     else
       return "blah blah blah blah blah"
